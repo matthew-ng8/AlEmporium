cd1e651d2f6bfae8120ae0d0cbaa1bbc3552ae94
William Ma <Figitobe@users.noreply.github.com>
2017-09-29 18:48:54 -0700

    B r o w n   B o i

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Uncle Al's Emporium</title>
+</head>
+<body>
+	<b1>
+		Welcome to Uncle Al's Emporium!
+	</b1>
+	<p>
+		Working with git is hard.
+	</p>
+</body>
+</html>
